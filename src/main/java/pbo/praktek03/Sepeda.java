d56b0c1eea66777442b7d8d8ea1743d414261460
Muhammad Nasrullah <muhammad.nasrullah1993@gmail.com>
2023-05-26 09:34:31 +0800

    Buat Method CetakInfo()

--- src/main/java/pbo/praktek03/Sepeda.java
+++ src/main/java/pbo/praktek03/Sepeda.java
@@ -8,4 +8,10 @@ public class Sepeda {
     String merk;
     String warna;
     String harga;
+    
+    void cetakInfo () {
+        System.err.println("Merk \t: "+merk+"\n"+
+                "Warna \t: "+warna+"\n"+
+                "Harga \t: "+harga);
+    }
 }
